bc0b0b15723e65b2458343e6115dd5482fe37afd
eddislucky <112645573+eddislucky@users.noreply.github.com>
2023-07-06 12:41:51 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// document.querySelector(".big-heading").innerHTML = "Producing healthy people."
+
+var numberOfGreet = document.querySelectorAll(".greet").length;
+
+for (i=0; i<numberOfGreet; i++) {
+    
+    document.querySelectorAll(".greet")[i].addEventListener("click", function(){
+        var greetInnerhtml = this.innerHTML;
+
+        switch (key) {
+            case a:
+                var greet1 = new alert("Hi");
+                break;
+        
+            default:
+                break;
+        }
+    })
+}
